5a1497452fdd0ab61cdc0f6fa01c641daf1d8e6d
Luis <luissilvar10@gmail.com>
2023-07-30 22:17:17 -0300

    Adicionando mais uma aula

---
+++ 2_conceitos_basicos/1_case_sensitive/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// O PHP não é Case Sensitive
+
+    echo "teste <br>";
+    eChO "testando 2 <br>";
+    ECHO "testando 3";
+
+// O PHP é Case Sensitive
+
+    $nome = "Luís";
+    
+    echo $nome;
+    echo "<br>";
+    echo $NOME;
+
+?>
