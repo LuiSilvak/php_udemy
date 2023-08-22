563f50dfeeee2ebb724e7126be2b652fad6e51f5
Luis <luissilvar10@gmail.com>
2023-08-21 22:38:39 -0300

    Adicionando arrays

---
+++ 3_tipos_de_dados/12_arrays/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $a = [1, 2, 3];
+    print_r($a);
+    echo "<br>";
+
+    echo $a[2];
+    echo "<br>";
+
+    $b = ["luis", 2, true];
+    print_r($b);
+    echo "<br>";
+
+    print_r($b[0]);
